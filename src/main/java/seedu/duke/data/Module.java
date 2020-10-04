4375ae4f52ce972cbbdceddd9adf6d9af1d4e157
wangqinNick <347656409nick@gmail.com>
2020-10-04 19:44:26 +0800

    Add EditModuleCommand

--- src/main/java/seedu/duke/data/Module.java
+++ src/main/java/seedu/duke/data/Module.java
@@ -1,33 +1,58 @@
 package seedu.duke.data;
 
-public class Module {
-    private String code;
+import seedu.duke.directory.Directory;
+import seedu.duke.directory.DirectoryLevel;
+
+public class Module extends Directory {
+    private String moduleCode;
+    private String title;
 
     public Module(String code) {
-        this.code = code;
+        this.moduleCode = code;
     }
 
-    public String getCode() {
-        return code;
+    public String getModuleCode() {
+        return moduleCode;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public void setModuleCode(String code) {
+        this.moduleCode = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Edits the title of the module.
+     *
+     * @param title
+     *  The title of the module
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Checks if one module has the same module code as another.
+     *
+     * @param moduleCode
+     *  The module code to check
+     * @return
+     *  <code>TRUE</code> if they are the same, and <code>FALSE</code> otherwise
+     */
+    public boolean isSameModule(String moduleCode) {
+        return this.moduleCode.equalsIgnoreCase(moduleCode);
+    }
+
+    @Override
+    public Directory getParent() {
+        return null;
+    }
+
+    @Override
+    public DirectoryLevel getLevel() {
+        return null;
     }
 }
 
-//import seedu.duke.directory.Directory;
-//import seedu.duke.directory.DirectoryLevel;
-//
-//public class Module extends Directory {
-//
-//    @Override
-//    public Directory getParent() {
-//        return null;
-//    }
-//
-//    @Override
-//    public DirectoryLevel getLevel() {
-//        return null;
-//    }
-//}
