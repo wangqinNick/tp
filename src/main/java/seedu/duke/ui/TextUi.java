bbaa2f8f5adfa78ee30650191a992db2aae9e83a
amalinasani <nuramalina.mdsani@gmail.com>
2020-10-11 21:13:12 +0800

    Add HelpCommand

--- src/main/java/seedu/duke/ui/TextUi.java
+++ src/main/java/seedu/duke/ui/TextUi.java
@@ -7,6 +7,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TextUi {
+    private static final String HELP_MESSAGE =
+              "Command                   Function\n"
+            + "help -------------------- View command list\n"
+            + "add -t [-by] ------------ Add task\n"
+            + "add -m ------------------ Add module\n"
+            + "edit -t ----------------- Edit a task's description\n"
+            + "edit -m ----------------- Edit a module\n"
+            + "del -t <task index>------ Delete a task\n"
+            + "del -m <module index>---- Delete a module\n"
+            + "list -t ----------------- List all tasks\n"
+            + "list -m ----------------- List all modules\n"
+            + "done <task index> ------- Mark task as done\n"
+            + "exit -------------------- Exit RaVi\n";
+
     public static void getTaskListMessage(ArrayList<Task> taskList){
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
@@ -20,5 +34,9 @@ public class TextUi {
             System.out.println(i + "." + module.getCode() + "\n" + module.getTitle());
             i++;
         }
+    }
+
+    public static String getHelpMessage(){
+        return HELP_MESSAGE;
     }
 }
