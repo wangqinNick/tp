387a6e482ea12dc9921aa0eade104829701434d6
Aseanseen <sean.lim.110@gmail.com>
2020-10-26 16:18:36 +0800

    Amend JUnit tests for TimeTableCommand

--- src/test/java/seedu/duke/command/timetable/TimeTableCommandTest.java
+++ src/test/java/seedu/duke/command/timetable/TimeTableCommandTest.java
@@ -1,10 +1,13 @@
 package seedu.duke.command.timetable;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.duke.command.CommandResult;
 import seedu.duke.data.Lesson;
 import seedu.duke.data.LessonType;
+import seedu.duke.data.Module;
+import seedu.duke.data.ModuleManager;
 import seedu.duke.data.TimeTableManager;
 import seedu.duke.exception.LessonInvalidTimeException;
 import seedu.duke.exception.TimeTableInitialiseException;
@@ -17,23 +20,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeTableCommandTest {
     static final int BAD_REPEAT_FREQ = 4;
-    static final int REPEAT_FREQ_ONCE = 0;
     static final int REPEAT_FREQ_WEEKLY = 1;
-    static final int REPEAT_FREQ_EVEN = 2;
-    static final int REPEAT_FREQ_ODD = 3;
     static final int BAD_LESSON_INDEX = 5;
-    static final int ONE_LESSON_COUNT = 1;
-    static final int EVEN_ODD_LESSON_COUNT = 7;
-    static final int WEEKLY_LESSON_COUNT = 13;
 
     static Lesson LESSON_1;
     static Lesson LESSON_1_OVERLAP;
     static Lesson LESSON_2;
-    static Lesson LESSON_3;
 
     static final String MOD_CODE_1 = "CS2113T";
     static final String MOD_CODE_2 = "CG2271";
     static final String MOD_CODE_3 = "MA1511";
+    static final String badViewFormat = "";
 
     static final DayOfWeek DAY_OF_WEEK = DayOfWeek.MONDAY;
     static final LocalTime LESSON_1_START = LocalTime.of(14,0);
@@ -42,11 +39,20 @@ public class TimeTableCommandTest {
     static final LocalTime OTHER_LESSON_END = LocalTime.of(10,0);
     static final LessonType LESSON_TYPE = LessonType.LECTURE;
 
+    @BeforeAll
+    static void setupUserMods() throws ModuleManager.DuplicateModuleException {
+        ModuleManager.clearModules();
+        ModuleManager.add(new Module(MOD_CODE_1));
+        ModuleManager.add(new Module(MOD_CODE_2));
+        ModuleManager.add(new Module(MOD_CODE_3));
+    }
+
     @BeforeEach
-    void setUp() throws TimeTableInitialiseException, LessonInvalidTimeException {
-        TimeTableManager.initialise(1);
+    void setupTimeTable_andLessons() throws LessonInvalidTimeException, TimeTableInitialiseException {
         LESSON_1 = new Lesson(MOD_CODE_1, LESSON_TYPE, DAY_OF_WEEK, LESSON_1_START, LESSON_1_END);
         LESSON_2 = new Lesson(MOD_CODE_2, LESSON_TYPE, DAY_OF_WEEK, OTHER_LESSON_START, OTHER_LESSON_END);
+        TimeTableManager.clearTimeTable();
+        TimeTableManager.initialise(1);
         TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_1, REPEAT_FREQ_WEEKLY);
         timeTableAddCommand.execute();
     }
@@ -63,39 +69,7 @@ public class TimeTableCommandTest {
         LESSON_1_OVERLAP = new Lesson(MOD_CODE_2, LESSON_TYPE, DAY_OF_WEEK, LESSON_1_START, LESSON_1_END);
         TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_1_OVERLAP, REPEAT_FREQ_WEEKLY);
         CommandResult commandResult = timeTableAddCommand.execute();
-        assertEquals(ExceptionMessage.MESSAGE_LESSON_OVERLAP, commandResult.feedbackToUser);
-    }
-
-    @Test
-    void addLesson_repeat_0_verifyOneLessonCreated() throws LessonInvalidTimeException {
-        LESSON_3 = new Lesson(MOD_CODE_3, LESSON_TYPE, DAY_OF_WEEK, OTHER_LESSON_START, OTHER_LESSON_END);
-        TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_3, REPEAT_FREQ_ONCE);
-        timeTableAddCommand.execute();
-        assertEquals(ONE_LESSON_COUNT, TimeTableManager.getCountOfLesson(LESSON_3));
-    }
-
-    @Test
-    void addLesson_repeat_1_verifyWeeklyLessonCreated() throws LessonInvalidTimeException {
-        LESSON_3 = new Lesson(MOD_CODE_3, LESSON_TYPE, DAY_OF_WEEK, OTHER_LESSON_START, OTHER_LESSON_END);
-        TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_3, REPEAT_FREQ_WEEKLY);
-        timeTableAddCommand.execute();
-        assertEquals(WEEKLY_LESSON_COUNT, TimeTableManager.getCountOfLesson(LESSON_3));
-    }
-
-    @Test
-    void addLesson_repeat_2_verifyEvenLessonCreated() throws LessonInvalidTimeException {
-        LESSON_3 = new Lesson(MOD_CODE_3, LESSON_TYPE, DAY_OF_WEEK, OTHER_LESSON_START, OTHER_LESSON_END);
-        TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_3, REPEAT_FREQ_EVEN);
-        timeTableAddCommand.execute();
-        assertEquals(7, TimeTableManager.getCountOfLesson(LESSON_3));
-    }
-
-    @Test
-    void addLesson_repeat_3_verifyOddLessonCreated() throws LessonInvalidTimeException {
-        LESSON_3 = new Lesson(MOD_CODE_3, LESSON_TYPE, DAY_OF_WEEK, OTHER_LESSON_START, OTHER_LESSON_END);
-        TimeTableAddCommand timeTableAddCommand = new TimeTableAddCommand(LESSON_3, REPEAT_FREQ_ODD);
-        timeTableAddCommand.execute();
-        assertEquals(EVEN_ODD_LESSON_COUNT, TimeTableManager.getCountOfLesson(LESSON_3));
+        assertEquals(ExceptionMessage.MESSAGE_LESSON_INVALID_TIME, commandResult.feedbackToUser);
     }
 
     @Test
@@ -103,5 +77,10 @@ public class TimeTableCommandTest {
         TimeTableDeleteCommand timeTableDeleteCommand = new TimeTableDeleteCommand(DAY_OF_WEEK, BAD_LESSON_INDEX);
         CommandResult commandResult = timeTableDeleteCommand.execute();
         assertEquals(ExceptionMessage.MESSAGE_LESSON_NOT_FOUND, commandResult.feedbackToUser);
+    }
+
+    @Test
+    void viewTimeTable_badViewFormat_IncorrectCommand_isReturned() {
+
     }
 }
