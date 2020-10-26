bd71ec753f5536d2839c0d71df2a9ec3f32ebfd4
Aseanseen <sean.lim.110@gmail.com>
2020-10-27 01:09:49 +0800

    Fix JUnit tests for TimeTableCommandParser

--- src/test/java/seedu/duke/parser/TimeTableCommandParserTest.java
+++ src/test/java/seedu/duke/parser/TimeTableCommandParserTest.java
@@ -8,6 +8,7 @@ import seedu.duke.command.IncorrectCommand;
 import seedu.duke.data.Module;
 import seedu.duke.data.ModuleManager;
 import seedu.duke.data.TimeTableManager;
+import seedu.duke.data.storage.InputOutputManager;
 import seedu.duke.exception.InvalidMatchException;
 import seedu.duke.exception.TimeTableInitialiseException;
 
@@ -30,21 +31,18 @@ public class TimeTableCommandParserTest {
     static final String DAY_OF_WEEK_2 = "MONDAY";
     static final String LESSON_TYPE = "LECTURE";
 
-    static final String WRONG_MOD_TIMETABLE_ADD_COMMAND_FORMAT = ADD_FORMAT + " " + MOD_CODE_2 + " "
-            + DAY_OF_WEEK_2 + " " + LESSON_START_TIME_3 + " " + LESSON_END_TIME + " "
-            + LESSON_TYPE + " " + REPEAT_FREQ;
-    static final String WRONG_LESSON_TIME_1_TIMETABLE_ADD_COMMAND_FORMAT = ADD_FORMAT + " " + MOD_CODE_1 + " "
-            + DAY_OF_WEEK_2 + " " + LESSON_START_TIME_1 + " " + LESSON_END_TIME + " " + LESSON_TYPE
-            + " " + REPEAT_FREQ;
-    static final String WRONG_LESSON_TIME_2_TIMETABLE_ADD_COMMAND_FORMAT = ADD_FORMAT + " " + MOD_CODE_1 + " "
-            + DAY_OF_WEEK_2 + " " + LESSON_START_TIME_2 + " " + LESSON_END_TIME + " " + LESSON_TYPE
-            + " " + REPEAT_FREQ;
-    static final String WRONG_DAY_TIMETABLE_ADD_COMMAND_FORMAT = ADD_FORMAT + " " + MOD_CODE_1 + " "
-            + DAY_OF_WEEK_1 + " " + LESSON_START_TIME_3 + " " + LESSON_END_TIME + " " + LESSON_TYPE + " "
-            + REPEAT_FREQ;
+    static final String WRONG_MOD_TIMETABLE_ADD_COMMAND_FORMAT = MOD_CODE_2 + " " + DAY_OF_WEEK_1 + " "
+            + LESSON_START_TIME_3 + " " + LESSON_END_TIME + " " + LESSON_TYPE + " " + REPEAT_FREQ;
+    static final String WRONG_LESSON_TIME_1_TIMETABLE_ADD_COMMAND_FORMAT = MOD_CODE_1 + " " + DAY_OF_WEEK_1 + " "
+            + LESSON_START_TIME_1 + " " + LESSON_END_TIME + " " + LESSON_TYPE + " " + REPEAT_FREQ;
+    static final String WRONG_LESSON_TIME_2_TIMETABLE_ADD_COMMAND_FORMAT = MOD_CODE_1 + " " + DAY_OF_WEEK_2 + " "
+            + LESSON_START_TIME_2 + " " + LESSON_END_TIME + " " + LESSON_TYPE + " " + REPEAT_FREQ;
+    static final String WRONG_DAY_TIMETABLE_ADD_COMMAND_FORMAT = ADD_FORMAT + " " + MOD_CODE_1 + " " + DAY_OF_WEEK_1
+            + " " + LESSON_START_TIME_3 + " " + LESSON_END_TIME + " " + LESSON_TYPE + " " + REPEAT_FREQ;
 
     @BeforeAll
     static void setupUserMods() throws ModuleManager.DuplicateModuleException, ModuleManager.ModuleNotFoundException {
+        InputOutputManager.loadNusModSave();
         ModuleManager.clearModules();
         ModuleManager.add(new Module(MOD_CODE_1));
     }
@@ -56,7 +54,7 @@ public class TimeTableCommandParserTest {
     }
 
     @Test
-    void badTimeTableCommandFlag_InvalidMatchException_isThrown() throws InvalidMatchException {
+    void badTimeTableCommandFlag_IncorrectCommand_isReturned() throws InvalidMatchException {
         Command command = TimeTableCommandParser.parseTimeTableCommand(WRONG_TIMETABLE_COMMAND_FORMAT);
         assertTrue(command instanceof IncorrectCommand);
     }
@@ -64,24 +62,24 @@ public class TimeTableCommandParserTest {
     @Test
     void bad_Module_TimeTableAddCommandFlag_ModuleNotFoundException_isThrown() {
         assertThrows(ModuleManager.ModuleNotFoundException.class,
-                () -> TimeTableCommandParser.parseTimeTableCommand(WRONG_MOD_TIMETABLE_ADD_COMMAND_FORMAT));
+            () -> TimeTableCommandParser.parseTimeTableAddCommand(WRONG_MOD_TIMETABLE_ADD_COMMAND_FORMAT));
     }
 
     @Test
     void bad_Lesson_Time_1_TimeTableAddCommandFlag_InvalidMatchException_isThrown() {
         assertThrows(InvalidMatchException.class,
-                () -> TimeTableCommandParser.parseTimeTableCommand(WRONG_LESSON_TIME_1_TIMETABLE_ADD_COMMAND_FORMAT));
+            () -> TimeTableCommandParser.parseTimeTableAddCommand(WRONG_LESSON_TIME_1_TIMETABLE_ADD_COMMAND_FORMAT));
     }
 
     @Test
     void bad_Lesson_Time_2_TimeTableAddCommandFlag_DateTimeParseException_isThrown() {
         assertThrows(DateTimeParseException.class,
-                () -> TimeTableCommandParser.parseTimeTableCommand(WRONG_LESSON_TIME_2_TIMETABLE_ADD_COMMAND_FORMAT));
+            () -> TimeTableCommandParser.parseTimeTableAddCommand(WRONG_LESSON_TIME_2_TIMETABLE_ADD_COMMAND_FORMAT));
     }
 
     @Test
     void bad_DateTime_TimeTableAddCommandFlag_IllegalArgumentException_isThrown() {
         assertThrows(IllegalArgumentException.class,
-                () -> TimeTableCommandParser.parseTimeTableCommand(WRONG_DAY_TIMETABLE_ADD_COMMAND_FORMAT));
+            () -> TimeTableCommandParser.parseTimeTableCommand(WRONG_DAY_TIMETABLE_ADD_COMMAND_FORMAT));
     }
 }
