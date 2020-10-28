1e19324c4b061883243026a5f63958b27d3f303e
f0fz <simjunyou99@gmail.com>
2020-10-28 22:22:47 +0800

    Added timetable filter command

--- src/main/java/seedu/duke/parser/LessonParser.java
+++ src/main/java/seedu/duke/parser/LessonParser.java
@@ -1,6 +1,7 @@
 package seedu.duke.parser;
 
 import seedu.duke.data.Lesson;
+import seedu.duke.data.LessonFilter;
 import seedu.duke.data.LessonType;
 import seedu.duke.data.ModuleManager;
 import seedu.duke.exception.LessonInvalidTimeException;
@@ -9,6 +10,7 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 
 import static seedu.duke.parser.TimeTableCommandParser.DAY_GROUP;
@@ -18,6 +20,7 @@ import static seedu.duke.parser.TimeTableCommandParser.MODULE_GROUP;
 import static seedu.duke.parser.TimeTableCommandParser.START_TIME_GROUP;
 
 public class LessonParser {
+    public static final String SKIP_PARAMETER_CHAR = "-";
     /**
      * Parses the user input for the Lesson parameters, to create a Lesson.
      *
@@ -51,4 +54,63 @@ public class LessonParser {
         Lesson newLesson = new Lesson(modString, lessonType, dayOfWeek, startTime, endTime);
         return newLesson;
     }
+
+
+    public static ArrayList<LessonFilter> parseFilterLesson(Matcher filterMatcher) throws
+            ModuleManager.ModuleNotFoundException, DateTimeParseException,
+            IllegalArgumentException {
+        String modString = filterMatcher.group(MODULE_GROUP).toUpperCase().trim();
+        String dayString = filterMatcher.group(DAY_GROUP).toUpperCase().trim();
+        String startTimeString = filterMatcher.group(START_TIME_GROUP).trim();
+        String endTimeString = filterMatcher.group(END_TIME_GROUP).trim();
+        String lessonTypeString = filterMatcher.group(LESSON_TYPE_GROUP).toUpperCase().trim();
+
+        ArrayList<LessonFilter> filterList = new ArrayList<>();
+
+        if (!modString.equals(SKIP_PARAMETER_CHAR)) {
+            // Check if modString is in module list
+            if (!ModuleManager.contains(modString)) {
+                throw new ModuleManager.ModuleNotFoundException();
+            } else {
+                LessonFilter modFilter = (l) -> l.getModuleCode().equals(modString);
+                filterList.add(modFilter);
+            }
+        }
+
+        if (!dayString.equals(SKIP_PARAMETER_CHAR)) {
+            // Convert dayString to DayOfWeek
+            DayOfWeek dayOfWeek = DayOfWeek.valueOf(dayString);
+            LessonFilter dayFilter = (l) -> l.getDay().equals(dayOfWeek);
+            filterList.add(dayFilter);
+        }
+
+        // Convert start, end strings to LocalTime
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("Hmm");
+
+        // Filter for start time. i.e. Filter lessons that start after HH:MM
+        if (!startTimeString.equals(SKIP_PARAMETER_CHAR)) {
+            LocalTime startTime = LocalTime.parse(startTimeString, time);
+            LessonFilter startFilter = (l) ->
+                (l.getStartTime().isAfter(startTime) || l.getStartTime().equals(startTime) );
+            filterList.add(startFilter);
+        }
+
+        // Filter for start time. i.e. Filter lessons that end before HH:MM
+        if (!endTimeString.equals(SKIP_PARAMETER_CHAR)) {
+            LocalTime endTime = LocalTime.parse(endTimeString, time);
+            LessonFilter endFilter = (l) ->
+                (l.getEndTime().isBefore(endTime) || l.getEndTime().equals(endTime));
+            filterList.add(endFilter);
+        }
+
+        if (!lessonTypeString.equals(SKIP_PARAMETER_CHAR)) {
+            // Convert lessonTypeString to lessonType
+            LessonType lessonType = LessonType.valueOf(lessonTypeString);
+            LessonFilter lessonTypeFilter = (l) -> l.getLessonType().equals(lessonType);
+            filterList.add(lessonTypeFilter);
+        }
+
+        return filterList;
+    }
+
 }
