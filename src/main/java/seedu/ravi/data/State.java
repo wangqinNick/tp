91dcd14072082b346ffa961e05ca50b1890a07e7
Aseanseen <sean.lim.110@gmail.com>
2020-11-09 05:42:06 +0800

    Allow undo for timetable commands

--- src/main/java/seedu/ravi/data/State.java
+++ src/main/java/seedu/ravi/data/State.java
@@ -6,11 +6,13 @@ public class State {
     private final String encodedSavedList;
     private final String encodedSavedMap;
     private final ArrayList<String> commandArrayList;
+    private final String encodedTimeTable;
 
-    public State(String encodedSavedList, String encodedSavedMap, ArrayList<String> commandArrayList) {
+    public State(String encodedSavedList, String encodedSavedMap, ArrayList<String> commandArrayList, String encodedTimeTable) {
         this.encodedSavedList = encodedSavedList;
         this.encodedSavedMap = encodedSavedMap;
         this.commandArrayList = commandArrayList;
+        this.encodedTimeTable = encodedTimeTable;
     }
 
     public String getEncodedSavedList() {
@@ -23,5 +25,9 @@ public class State {
 
     public ArrayList<String> getEditTypeCommandArrayList() {
         return commandArrayList;
+    }
+
+    public String getEncodedTimeTable() {
+        return encodedTimeTable;
     }
 }
